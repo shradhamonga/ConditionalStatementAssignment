e53ba2362d6b1678b0319d1f912e7807491b1549
shradhamonga <110901146+shradhamonga@users.noreply.github.com>
2025-10-22 17:46:14 +0530

    Add files via upload

---
+++ loopsAssignment.java
@@ -0,0 +1,14 @@
+
+public class loopsAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i=1; i <=100; i++) {
+			if(i%5==0 ) continue;
+			System.out.println(i);
+			
+		}
+	}
+
+}
